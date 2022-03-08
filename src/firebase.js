3c2f5e2679c4fed7ffefe0c88ee089dbae924855
Emily.Garcia <emilikimberly1@gmail.com>
2022-03-07 22:25:05 -0500

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbhu2asvVLOg4vU-bzZUiqVaCki6DPIJk",
+  authDomain: "territorios-abb75.firebaseapp.com",
+  projectId: "territorios-abb75",
+  storageBucket: "territorios-abb75.appspot.com",
+  messagingSenderId: "869597927154",
+  appId: "1:869597927154:web:40864f0362add08f5d626b",
+  measurementId: "G-SM7CPB0DK3"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
