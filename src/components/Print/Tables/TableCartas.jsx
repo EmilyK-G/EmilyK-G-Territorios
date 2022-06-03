7812ba43cf92092008e6da7c67180219fd6b2127
Emily.Garcia <emilikimberly1@gmail.com>
2022-06-02 21:59:14 -0400

    Fixed cartas fetch bug

--- src/components/Print/Tables/TableCartas.jsx
+++ src/components/Print/Tables/TableCartas.jsx
@@ -25,21 +25,23 @@ const TableCartas = forwardRef((props, cartasRef)=>{
         house.Resident.map((res)=>(
             residentsName.push(res.Name)
         ))
-        const residentsShift = residentsName.shift();
         
         return (
             <>
             <tr key={i}>
                 <td rowSpan={residentsName.length === 1 ? residentsName.length : residentsName.length + 1}>{house.House} {house.Street}</td>
-                <td>{residentsName[0] ? residentsShift : ""}</td>
+                {residentsName.length === 1 ?
+                <><td>{residentsName[0]}</td>
                 <td> </td>
                 <td> </td>
                 <td> </td>
+                <td> </td></> : null}
             </tr>
             {residentsName.length > 1 ? 
-                residentsName.map((res)=>{
-                    return <tr>
+                residentsName.map((res, ii)=>{
+                    return <tr key={ii}>
                         <td>{res}</td>
+                        <td> </td>
                         <td> </td>
                         <td> </td>
                         <td> </td>
@@ -54,7 +56,7 @@ const TableCartas = forwardRef((props, cartasRef)=>{
             <table className="table table-bordered" ref={cartasRef}>
                 <thead>
                     <tr>
-                        <th colSpan={5}>Territorio # {terrSelected} -Cartas</th>
+                        <th colSpan={6}>Territorio # {terrSelected} -Cartas</th>
                     </tr>
                     <tr>
                         <th>Dirección</th>
@@ -62,6 +64,7 @@ const TableCartas = forwardRef((props, cartasRef)=>{
                         <th>1ra Carta</th>
                         <th>2ra Carta</th>
                         <th>3ra Carta</th>
+                        <th>4ta Carta</th>
                     </tr>
                 </thead>
                 <tbody>
